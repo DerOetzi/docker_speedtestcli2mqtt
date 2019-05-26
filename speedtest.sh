8f4c9dfcd593368034bccf129b8a22400e400faf
Johannes DerOetzi Ott <info@johannes-ott.net>
2019-05-26 18:26:53 +0200

    Escape output string

--- speedtest.sh
+++ speedtest.sh
@@ -8,6 +8,6 @@ echo "Current timestamp: $timestamp"
 output=$(speedtest-cli --json)
 echo "Output: $output"
 
-mosquitto_pub -h $MQTT_BROKER -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASSWORD -d -t $MQTT_TOPIC -m $output
+mosquitto_pub -h $MQTT_BROKER -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASSWORD -d -t $MQTT_TOPIC -m "$output"
 
 echo "New speedtest sent"
